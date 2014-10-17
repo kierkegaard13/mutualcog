4b0549b8f9b2d777cf60437b35c2525906f03744
kierkegaard13 <kierkegaard13@gmail.com>
2014-10-17 15:02:28 -0500

    Post searches now work

--- app/controllers/search.php
+++ app/controllers/search.php
@@ -12,6 +12,29 @@ class Search extends BaseController {
 		return $view;
 	}
 
+	public function parseSearch(&$tag_str,&$search_string,$word){
+		$split_string = preg_split($word,$search_string);
+		if(sizeof($split_string) > 1){  //found words before keyword
+			$search_string = $split_string[1];
+		}else{
+			$search_string = $split_string[0];
+		}
+		$tag_str = explode(',',$search_string);
+		foreach($tag_str as $index=>$tag){  
+			if(sizeof(explode(' ',trim($tag))) > 1){
+				$split_tag = explode(' ',trim($tag));
+				$tag_str[$index] = $split_tag[0];
+				$end_str = join(' ',array_splice($tag_str,0,$index - 1));
+				$tag_str = array_splice($tag_str,$index + 1);
+				$split_tag = array_splice($split_tag,0,1);
+				$split_tag = join(' ',$split_tag); 
+				$search_string = (sizeof($split_string) > 1) ? $split_string[0] . ' ' . $split_tag . ' ' . $end_str: $split_tag . ' ' . $end_str;
+				break;
+			}
+		}
+		$search_string = $split_string[0];
+	}
+
 	public function getResult($search_string,$second = null,$third = null){
 		$view = View::make('results');
 		$curr_page = Session::get('curr_page');
@@ -19,17 +42,17 @@ class Search extends BaseController {
 		$chats = '';
 		$users = '';
 		$communities = '';
-		$prime_keywords = ['posts','communities','users'];
-		$post_af_keywords = [' in ',' about ',' by '];
-		$post_bf_keywords = ['sfw','nsfw','live','static'];
-		$user_keywords = [' named ',' who like ',' near '];
-		$com_keywords = [' similar to ',' named ',' concerning '];
+		$prime_keywords = ["/\bposts/i","/\bcommunities/i","/\busers/i"];
+		$post_af_keywords = ["/\bin/i","/\babout/i","/\bby/i"];
+		$post_bf_keywords = ["/\bsfw/i","/\bnsfw/i","/\blive/i","/\bstatic/i"];
+		$user_keywords = ["/\bnamed/i","/\blike/i","/\bnear/i"];
+		$com_keywords = ["/\bnamed/i","/\bsimilar to/i","/\bconcerning/i"];
 		$search_string = htmlentities($search_string);
 		$found = -1;
 		foreach($prime_keywords as $key => $word){  //split search based on primary keywords
-			if(strpos($search_string,$word) !== false){
+			if(preg_match($word,$search_string)){
 				$found = $key;
-				$split_string = explode($word,$search_string);
+				$split_string = preg_split($word,$search_string);
 				break;
 			}
 		}
@@ -40,22 +63,22 @@ class Search extends BaseController {
 					$num_found = 0;
 					if(sizeof($split_string) > 1){  //keywords before posts found
 						foreach($post_bf_keywords as $key=>$word){
-							if(strpos($split_string[0],$word) !== false){
+							if(preg_match($word,$split_string[0])){
 								switch($key){
 									case 0:  //sfw
 										$chats = $chats->wherensfw(0);
 										$num_found++;
 										break;
 									case 1:  //nsfw
-										$chats = ($num_found == 0) ? $chats->wherensfw(1) : $chats->orWherensfw(1);
+										$chats = ($num_found == 0) ? $chats->wherensfw(1) : $chats->orWhere('nsfw',1);
 										$num_found++;
 										break;
 									case 2:  //live
-										$chats = ($num_found == 0) ? $chats->wherelive(1) : $chats->orWherelive(1);
+										$chats = ($num_found == 0) ? $chats->wherelive(1) : $chats->orWhere('live',1);
 										$num_found++;
 										break;
 									case 3:  //static
-										$chats = ($num_found == 0) ? $chats->wherelive(0) : $chats->orWherelive(0);
+										$chats = ($num_found == 0) ? $chats->wherelive(0) : $chats->orWhere('live',0);
 										$num_found++;
 										break;
 									default:break;
@@ -70,39 +93,41 @@ class Search extends BaseController {
 						if(strpos($search_string,$word) !== false){   
 							switch($key){    
 								case 0:  //in
-									$split_string = explode($word,$search_string);
-									if(sizeof($split_string) > 1){  //found words before keyword
-										$search_string = $split_string[1];
-									}else{
-										$search_string = $split_string[0];
-									}
-									$tag_str = explode(',',$search_string);
-									foreach($tag_str as $index=>$tag){  
-										if(sizeof(explode(' ',$tag)) > 1){
-											$split_tag = explode(' ',$tag);
-											$tag_str[$index] = $split_tag[0];
-											$tag_str = array_splice($tag_str,$index + 1);
-											$split_tag = array_splice($split_tag,0,1);
-											$split_tag = join(' ',$split_tag); 
-											$search_string = (sizeof($split_string) > 1) ? $split_string[0] . ' ' . $split_tag : $split_tag;
-											break;
-										}
-									}
+									$this->parseSearch($tag_str,$search_string,$word);
 									$chats = $chats->whereHas('tags',function($query)use($tag_str){
 										$query->where(function($q)use($tag_str){
 											foreach($tag_str as $key=>$tag){
 												if($key == 0){
-													$q->where('name','=',$tag);
+													$q->where('name','LIKE','%'.$tag.'%');
 												}else{
-													$q->orWhere('name','=',$tag);
+													$q->orWhere('name','LIKE','%'.$tag.'%');
 												}
 											}
 										});
 									});
 									break;
 								case 1:  //about
+									$this->parseSearch($tag_str,$search_string,$word);
+									$chats = $chats->where(function($query)use($tag_str){
+										foreach($tag_str as $key=>$tag){
+											if($key == 0){
+												$query->where(function($q)use($tag){
+													$q->where('title','LIKE','%'.$tag.'%');
+													$q->orWhere('raw_details','LIKE','%'.$tag.'%');
+												});
+											}else{
+												$query->orWhere(function($q)use($tag){
+													$q->where('title','LIKE','%'.$tag.'%');
+													$q->orWhere('raw_details','LIKE','%'.$tag.'%');
+												});
+											}
+										}
+									});
 									break;
 								case 2:  //by
+									$this->parseSearch($tag_str,$search_string,$word);
+									$tag_str = $tag_str[0];
+									$chats = $chats->whereadmin($tag_str);
 									break;
 								default:break;
 							}
